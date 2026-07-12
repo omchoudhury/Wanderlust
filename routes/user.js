742b1e02bb0e25ad25b9cf4dad1949e4a24661b2
omchoudhury <omchoudhury7890@gmail.com>
2026-07-12 12:44:35 +0530

    Rename WrapAsync to wrapasync

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
-const wrapasync = require("../utils/wrapasync.js");
+const WrapAsync = require("../utils/wrapasync.js");
 
 const usersController = require("../controllers/users.js");
 
 //render signup form
 router.route("/signup")
 .get(usersController.renderSignupForm)
-.post(wrapasync(usersController.signup));
+.post(WrapAsync(usersController.signup));
 
 
 //render login form and login post requests
